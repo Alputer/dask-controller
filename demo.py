2cf25ba19729ee3a01d126a527f60be7e9b8fa6a
Alputer <alp.tuna.453@gmail.com>
2024-07-15 17:08:25 +0200

    Update scheduler address fetching and analysis.py

--- demo.py
+++ demo.py
@@ -9,7 +9,6 @@ import os
 # Code given below will be the image
 
 DASK_SCHEDULER_URI = os.getenv("DASK_SCHEDULER_URI", "tcp://127.0.0.1:8080")
-print(DASK_SCHEDULER_URI)
 
 client = dask.distributed.Client(DASK_SCHEDULER_URI)
 
@@ -26,7 +25,7 @@ def save_result(data, filename):
 data = load_data('data.npy')
 
 # Convert to Dask array
-x = dask.array.from_delayed(data, dtype=np.float64, shape=(10000, 10000))
+x = dask.array.from_delayed(data, dtype=np.float64, shape=(100, 100))
 
 # Perform computations
 y = x + x.T
@@ -36,5 +35,5 @@ z = y[::2, 5000:].mean(axis=1)
 result = z.compute()
 
 # Save the result using a Dask worker
-output_filename = save_result(result, './output.txt')
+output_filename = save_result(result, './output/output.txt')
 output_filename.compute()
--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+helm delete dask
+sleep 3
+helm install dask dask/dask -f values.yaml --version 2024.1.1
+sleep 3
 docker rmi dask-demo:alputer
 docker build -t dask-demo:alputer .
 kind load docker-image dask-demo:alputer
 kubectl delete job dask-demo-job
 # For HostPath
-kubectl apply -f job.yaml
+DASK_SCHEDULER_URI=$(kubectl get services dask-scheduler | awk 'NR==2 {print "tcp://" $3 ":" substr($5,0,4)}')
+export DASK_SCHEDULER_URI
+envsubst < job.yaml | kubectl apply -f -
 
 # For persistent volume
 #kubectl apply -f persistent_volume.yaml
